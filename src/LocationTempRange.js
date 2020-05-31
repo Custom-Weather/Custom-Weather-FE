8eb57a8c993e1b101275a290c8997bb356e19652
David Holtkamp <david.hholtkamp@gmail.com>
2020-05-30 20:34:48 -0600

    Create current high low temperature component

--- src/LocationTempRange.js
+++ src/LocationTempRange.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import './LocationTempRange.css';
+import LocationForm from './LocationForm';
 
-const LocationTempRange = () => {
+
+const LocationTempRange = ({ current, high, low }) => {
   return (
-    
+    <div className='location-temp-range'>
+      <h4> Today's Temperature Ranges </h4>
+      <p>Current: {current}</p>
+      <p>High: {high}</p>
+      <p>Low: {low}</p>
+    </div>
   )
 }
+
+export default LocationTempRange;
