4980dfe3dff147bbb647d072c039c08369680ea6
David Holtkamp <david.hholtkamp@gmail.com>
2020-06-02 13:03:38 -0600

    Format temperature display

--- src/LocationTempRange.js
+++ src/LocationTempRange.js
@@ -7,9 +7,9 @@ const LocationTempRange = ({ current, high, low }) => {
   return (
     <div className='location-temp-range'>
       <h4> Today's Temperature Ranges </h4>
-      <p>Current: {current}</p>
-      <p>High: {high}</p>
-      <p>Low: {low}</p>
+      <p>Current: {current}°F</p>
+      <p>High: {high}°F</p>
+      <p>Low: {low}°F</p>
     </div>
   )
 }
