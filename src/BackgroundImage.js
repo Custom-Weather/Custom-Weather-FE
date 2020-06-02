16b894c2802e5cbbb5206df40205c4929d00b299
Kathleen Carroll <khakicarroll@comcast.net>
2020-06-02 12:57:09 -0600

    Update default images

--- src/BackgroundImage.js
+++ src/BackgroundImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './BackgroundImage.css';
 import thunderstorm from './images/storm.gif'
-import rain from './images/rain.gif'
+import rain from './images/rain-2.gif'
 import drizzle from './images/drizzle.gif'
 import clear from './images/clear.gif'
 import clouds from './images/clouds.gif'
