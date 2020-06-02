c4f54009f4e23412a5bb8606b9cfd335d02bd040
Kathleen Carroll <khakicarroll@comcast.net>
2020-06-02 13:29:01 -0600

    Start work on nav bar selection functionality

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Navbar.css';
-// import styled from "styled-components";
 
 const Navbar = () => {
+  const widgets = [
+    'SunriseSunset',
+    'HourlyForecast',
+    'BackgroundImage',
+    'LocationTempRange',
+    'LocationWelcome',
+    'SpotifyPlayer'
+  ]
+
+  const widgetList = widgets.map(widget => {
+    return (
+      <button>{widget}</button>
+    )}
+  )
   return (
     <div className='side-bar'>
       <h2>Widgets</h2>
+      {widgetList}
     </div>
   )
 }
