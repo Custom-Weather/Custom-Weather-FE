421208e1781d555603930a37ecdd13b685f6a35f
Kathleen Carroll <khakicarroll@comcast.net>
2020-06-03 00:11:06 -0600

    Update styling and display of data for high low current component

--- src/LocationTempRange.js
+++ src/LocationTempRange.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './LocationTempRange.css';
 import LocationForm from './LocationForm';
+import lowTemp from './images/low-temp.png'
+import highTemp from './images/high-temp.png'
 
 
 const LocationTempRange = ({ current, high, low }) => {
   return (
     <div className='location-temp-range'>
-      <h4> Today's Temperature Ranges </h4>
-      <p>Current: {current}</p>
-      <p>High: {high}</p>
-      <p>Low: {low}</p>
+      <h1 className='current'> {current}</h1>
+      <div className='temp-container'>
+        <h4 className='high'><img id='high-temp-img' src={highTemp}></img>{high}</h4>
+        <h4 className='low'><img id='low-temp-img' src={lowTemp}></img>{low}</h4>
+      </div>
     </div>
   )
 }
