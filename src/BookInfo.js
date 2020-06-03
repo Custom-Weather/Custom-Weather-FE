6ee147b944733d1478a8d27864dec48adc14a2c4
Kathleen Carroll <khakicarroll@comcast.net>
2020-06-03 17:37:08 -0600

    clean up merge

--- src/BookInfo.js
+++ src/BookInfo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import BookDetail from './BookDetail';
 import './BookInfo.css';
 
@@ -14,15 +13,6 @@ const BookInfo = ({ notifications }) => {
   return (
     <div className='book-info'>
       <p>{books}</p>
-=======
-import './BookInfo.css';
-
-const BookInfo = ({ book }) => {
-  return (
-    <div className='book-info'>
-      <h4> Read a book for once in your life: </h4>
-      <p>{book}</p>
->>>>>>> 03fb069ee63b06709a3bd77691d8060741a6ef03
     </div>
   )
 }
--- src/EventInfo.js
+++ src/EventInfo.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import EventDetail from './EventDetail';
 import './EventInfo.css';
 
@@ -15,15 +14,6 @@ const EventInfo = ({ notifications }) => {
   return (
     <div className='event-info'>
       {events}
-=======
-import './EventInfo.css';
-
-const EventInfo = ({ name, link }) => {
-  return (
-    <div className='event-info'>
-      <h4> Get out of your comfort zone and go to an event: </h4>
-      <p><a href="link"> name</a></p>
->>>>>>> 03fb069ee63b06709a3bd77691d8060741a6ef03
     </div>
   );
 };
